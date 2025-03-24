edd1fee81190cf01151aaaab71eae82e1bda4b24
King12-D <mickyyoung660@gmail.com>
2025-03-24 12:27:03 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *KING-AI*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/King12-D_
+❒ *Author:* _wa.me/2347086613483_
+❒ *YT:* _https://youtube.com/@kingdav660_
 └────────────⭓
 `
 
@@ -134,7 +134,7 @@ SESSION-ID ==> ${Scan_Id}
     await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
 
 
-    //// MADE BY ITXWASI
+    //// MADE BY KING_DAV
 
 });
 
